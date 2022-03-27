cf1d0132837aa1cc612960dbc8958bb18fbb305e
Baptiste71 <planchonbaptiste@gmail.com>
2022-03-27 10:22:04 +0200

    mise a jour controller sauces

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,10 +1,10 @@
 // Importation du modele Sauce
 
 const allSauces = require("../models/Sauces");
-const jsonWT = require("jsonwebtoken");
-const req = require("express/lib/request");
-const res = require("express/lib/response");
-const Sauces = require("../models/Sauces");
+
+// Importation fs 'file system' ici utilisé pour le controller delete
+
+const fs = require("fs");
 
 // controller pour la route get
 
@@ -29,7 +29,7 @@ exports.getJustOneElement = (req, res, next) => {
 exports.addElement = (req, res, next) => {
   const saucesObject = JSON.parse(req.body.Sauces);
   delete saucesObject._id;
-  const addSauce = new Sauces({
+  const addSauce = new allSauces({
     ...saucesObject,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
     likes: 0,
@@ -45,4 +45,64 @@ exports.addElement = (req, res, next) => {
 
 // controller pour la route put
 
-exports.updateElement = (req, res, next) => {};
+exports.updateElement = (req, res, next) => {
+  const saucesObject = req.file
+    ? {
+        ...JSON.parse(req.body.Sauces),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+      }
+    : { ...req.body };
+
+  allSauces
+    .updateOne({ _id: req.params.id }, { ...saucesObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Sauces modifiée !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+// controller pour la route delete
+
+exports.deleteElement = (req, res, next) => {
+  allSauces
+    .findOne({ _id: req.params.id })
+    .then((sauces) => {
+      const filename = sauces.imageUrl.split("/images/")[1];
+      fs.unlink(`images/${filename}`, () => {
+        allSauces
+          .deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: "Sauce supprimée !" }))
+          .catch((error) => res.status(400).json({ error }));
+      });
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+// controller pour la route post likes / dislikes
+
+exports.likeElement = (req, res, next) => {
+  const sauces = req.params.id;
+  const user = req.body.userId;
+  const likes = req.body.like;
+
+  if (likes) {
+    // Dans le cas d'un like
+
+    allSauces
+      .updateOne({ _id: sauces }, { usersLiked: user, likes: +1 })
+      .then(() => res.status(200).json({ message: "J'aime cette sauce !" }))
+      .catch((error) => res.status(400).json({ error }));
+  } else if (likes) {
+    // Dans le cas d'un dislike
+
+    allSauces
+      .updateOne({ _id: sauces }, { usersLiked: user, dislikes: -1 })
+      .then(() => res.status(200).json({ message: "J'aime pas cette sauce !" }))
+      .catch((error) => res.status(400).json({ error }));
+  } else {
+    // Dans le cas ou like n'est pas attribué
+
+    allSauces
+      .updateOne({ _id: sauces }, { usersLiked: user })
+      .then(() => res.status(200).json({ message: "J'aime pas cette sauce !" }))
+      .catch((error) => res.status(400).json({ error }));
+  }
+};
